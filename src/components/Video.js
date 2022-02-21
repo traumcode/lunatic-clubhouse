7f8215dc76f274291430f1b29021d83f6287a00f
Silviu Bocsa <silviubocsa@gmail.com>
2022-02-21 23:39:25 +0200

    upload

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,36 +1,22 @@
 import React, {useState, useEffect } from "react";
 
 const Video = (props) => {
-	const [src] = useState(props.videos[3])
+	const [src] = useState(props.videos[0])
 
 	useEffect(() => {
 		let element = document.getElementById('landingVideo')
-		let count = 0
-			element.addEventListener("canplay", function () {
-				if(count === 0) {
-					setTimeout(function () {
-						element.setAttribute('src', props.videos[4])
-						count = 1
-					}, 1900 )
-				}
-				// if(count === 0)
-				// 	setTimeout(function () {
-				// 		element.currentTime = 10;
-				// 		count = 1
-				// 	}, 1900 )
-
-			})
-
-
-
-
+		element.addEventListener("loadedmetadata", function () {
+			if (element.buffered.length === 0) return
+			const buffered = element.buffered.end(0) - element.buffered.start(0)
+			console.log(` ${buffered}`)
+		})
 	}, [])
 
 	return (
 		<div
 			dangerouslySetInnerHTML={{
 			__html: `
-					<video id="landingVideo" class="app__backgroundVideo landing-video" playsinline autoplay loop muted>
+					<video id="landingVideo" preload="metadata" class="app__backgroundVideo landing-video" playsinline autoplay loop muted>
       					<source src=${src} type="video/mp4" id='landingVideoSource'/>
       						Your browser does not support the video tag.
 					</video>`,
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,21 +1,13 @@
 import React from 'react';
 import Video from '../components/Video';
-import Videolanding from "../resources/animation1.mp4"
-import Videolanding2 from "../resources/animation2.mp4"
-import Videolanding3 from "../resources/animation3.mp4"
-import Videolanding4 from "../resources/animation4.mp4"
-import Videolanding5 from "../resources/animation2 (1).mp4"
-
-
+import Videolanding from "../resources/animation7.mp4"
+import Videolanding2 from "../resources/animation4.mp4"
 
 
 function Landing(props) {
     const myVideos = [
         Videolanding,
         Videolanding2,
-        Videolanding3,
-        Videolanding4,
-        Videolanding5
     ]
 
 
