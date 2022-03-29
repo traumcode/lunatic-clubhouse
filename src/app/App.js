f1391b5148128a8cb1bb6048c82a6942048bcd3c
Silviu Bocsa <70107862+traumcode@users.noreply.github.com>
2022-03-29 08:59:42 +0300

    updated

--- src/app/App.js
+++ src/app/App.js
@@ -1,15 +1,22 @@
-import './App.scss';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Landing from "../pages/Landing";
+import React, {useEffect, useRef} from "react";
+
+import OnePageApp from "../components/OnePageApp";
+import Privacy from "../components/Privacy";
+import Terms from "../components/Terms";
+
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
-  return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing/>}/>
-        </Routes>
-      </BrowserRouter>
-  );
+
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<OnePageApp/>}/>
+                <Route path='/privacy' element={<Privacy/>}/>
+                <Route path='/terms' element={<Terms/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
