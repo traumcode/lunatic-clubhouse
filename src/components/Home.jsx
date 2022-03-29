d01b368bac0e553aa6c6988008b8371546a792aa
Silviu Bocsa <70107862+traumcode@users.noreply.github.com>
2022-03-29 15:02:26 +0300

    update

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,18 +4,18 @@ import Video1 from '../assets/intro_video.mp4'
 export default function Home() {
     return (
         <div className="home">
-            <div className="container">
-                <div className="content">
-                    <button className='button-64' role='button'>
-                        <span className="text">Getting Started</span>
-                    </button>
-                    <button className='button-64' role='button'>
-                        <span className="text">Whitepaper</span>
-                    </button>
-                    <video autoPlay muted loop id='lunaticVideo' className='video-64'>
-                        <source src={Video1} type='video/mp4'/>
-                    </video>
-                </div>
+            <div className='video-wrapper'>
+                <video playsInline autoPlay muted loop poster='../assets/casino.jpeg'>
+                    <source src={Video1} type='video/mp4'/>
+                </video>
+            </div>
+            <div className='button-container'>
+                <button className='button-64' role='button'>
+                    <span className="text">Getting Started</span>
+                </button>
+                <button className='button-64' role='button'>
+                    <span className="text">Whitepaper</span>
+                </button>
             </div>
         </div>
     );
